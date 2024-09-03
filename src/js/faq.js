fa46e34598b72ebc08bfac9b987fea920b042db8
dpartner <d.partner.ua@gmail.com>
2024-09-03 10:21:53 +0300

    faq answer

---
+++ src/js/faq.js
@@ -0,0 +1,7 @@
+export default function shownFaqAnswer(ev, { faqAnswer }) {
+  ev.preventDefault();
+  console.log(ev);
+  console.log(faqAnswer);
+  console.log(ev.target.nextElementSibling);
+  ev.target.nextElementSibling.classList.toggle('is-show');
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import toogleMobMenu from './js/mob-menu';
+import shownFaqAnswer from './js/faq';
 
 const domElements = {
   mobMenuButton: document.querySelector('.mob-menu-button'),
   mobMenu: document.querySelector('.mobile-menu'),
+  faqList: document.querySelector('.faq-list'),
+  faqAnswer: document.querySelectorAll('.faq-list-item-answer'),
 };
 
 domElements.mobMenuButton.addEventListener('click', () => {
   toogleMobMenu(domElements);
+});
+
+domElements.faqList.addEventListener('click', ev => {
+  shownFaqAnswer(ev, domElements);
 });
