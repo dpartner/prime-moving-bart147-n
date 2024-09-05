1ef92cce8d6b6e0277d40d675d25bd0eff3f8199
dpartner <d.partner.ua@gmail.com>
2024-09-05 21:03:36 +0300

    no focus mobile

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,8 @@
 export default function shownFaqAnswer(ev, { faqAnswer }) {
   ev.preventDefault();
+  if (ev.target.classList.contains('faq-list-item-answer')) {
+    console.log(2);
+  }
   console.log(ev);
   console.log(faqAnswer);
   console.log(ev.target.nextElementSibling);
