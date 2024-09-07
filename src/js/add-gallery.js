48261e64bef8f78cca7062062446929b418645ea
dpartner <d.partner.ua@gmail.com>
2024-09-07 17:21:03 +0300

    dynamic picture bug

--- src/js/add-gallery.js
+++ src/js/add-gallery.js
@@ -6,10 +6,10 @@ export const addGalleryImg = GalleryLibrary => {
                   <img
                     class="experience-gallery-img"
                     srcset="
-                      ./img/exp-gallery/${img.defaultImg} 1x,
-                      ./img/exp-gallery/${img.retinaImg} 2x
+                      ${img.defaultImg} 1x,
+                      ${img.retinaImg} 2x
                     "
-                    src="./img/exp-gallery/gallery-mob-1@1x.webp"
+                    src="${img.defaultImg}"
                     alt="${img.alt}"
                   />
                 </a>
